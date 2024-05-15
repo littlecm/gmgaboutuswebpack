580c95d5e481472df5bc87937a88da5c7f5ee760
littlecm <clittle.norml@gmail.com>
2024-05-15 10:24:50 -0400

    Update webpack.widget.config.js

--- webpack.widget.config.js
+++ webpack.widget.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./widget.js'], // Make sure this is the correct path to your entry file
+  entry: './widget.js', // Ensure this is the correct path to your entry file
   output: {
     path: path.resolve(__dirname, 'public/widget'),
     filename: 'widget-bundle.js',
-    library: 'SpecialsWidget',
+    library: 'AboutUsWidget',
     libraryTarget: 'umd',
     publicPath: '/widget/',
     globalObject: 'this'
