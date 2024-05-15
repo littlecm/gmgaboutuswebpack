dcdcb5d3b59cf0062bac08b2796627ccb89d94b6
littlecm <clittle.norml@gmail.com>
2024-05-15 11:00:09 -0400

    Update webpack.widget.config.js

--- webpack.widget.config.js
+++ webpack.widget.config.js
@@ -26,10 +26,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          {
-            loader: 'css-loader',
-            options: { importLoaders: 1 }
-          },
+          'css-loader',
           'postcss-loader'
         ]
       }
